8b84e94fb39c0f4427796ebcf8d026dfee09466f
lawl <lawrencetibayan01@gmail.com>
2025-11-23 18:04:01 +0800

    Enhance Account class with username and password
    Added username and password fields to the Account class.

--- Account.java
+++ Account.java
@@ -1,5 +1,17 @@
 package components;
 
 public class Account {
-    
+    private String username;
+    private String password;
+// Optional: for budget tracking
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+
 }
